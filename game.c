905666a4fa9092e72a58e9d35aac25e364970141
Ch1nA7 <825819701@qq.com>
2020-11-19 21:31:06 +0800

    11_19

---
+++ game.c
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include"game.h"
+void InitBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			board[i][j] = ' ';
+		}
+
+	}
+}
+
+//void DisplayBoard(char board[ROW][COL], int row, int col)
+//{
+//	int i = 0;
+//	for (i = 0; i < row; i++)
+//	{
+//		//1.打印一行数据
+//		printf(" %c | %c | %c \n", board[i][0], board[i][1], board[i][2]);
+//		//2.打印分割行
+//		if (i < row - 1)
+//		printf("---|---|---\n");
+//	}
+//
+//}
+
+
+void DisplayBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		//1.打印一行数据
+		int j = 0;
+		for (j = 0; j < col; j++)
+		{
+			printf(" %c ", board[i][j]);
+			if (j < col -1)
+			printf("|");
+		}
+		printf("\n");
+		//2.打印分割行
+		if (i < row - 1)
+		{
+			for (j = 0; j < col; j++)
+			{
+				printf("---");
+				if (j <col-1)
+				printf("|");
+			}
+			printf("\n");
+		}
+	}
+
+}
+
+void PlayerMove(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	printf("玩家走：>\n");
+	while (1)
+	{
+		printf("请输入要走的坐标：>");
+		scanf("%d%d", &x, &y);
+		//判断x,y坐标的合法性
+		if (x >= 1 && x <= row  &&  y >= 1 && y <= col)
+		{
+			if
+		}
+		else
+		{
+			printf("坐标非法，请重新输入");
+		}
+	}
+
+}
+
